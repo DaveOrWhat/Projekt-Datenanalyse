cfd18919deabf47ad11a663b9d3093795575e66f
David Blanck <david_blanck@outlook.com>
2024-11-30 01:02:15 +0100

    Legende hinzugefügt

--- Test.py
+++ Test.py
@@ -31,8 +31,9 @@ def monthToInt(month):
         case "Dezember":
             return 12
 
-gesamtErzeugung = []
-prozentualeErzeugung = []
+steinkohleErzeugung = []        #monatliche Erzeugung von Steinkohlestrom
+gesamtErzeugung = []            #monatliche Gesamtproduktion an Strom
+prozentualeErzeugung = []       #prozentuale Erzeugung von Steinkohlestrom an der Gesamtproduktion pro Monat
 jahre = []
 
 data = open('RAW/43311-0002_Steinkohle.csv', 'r')
@@ -41,11 +42,15 @@ for zeile in data:
     if zeile.startswith('2'):
         temp = zeile.split(";")                                                     #die Zeile am Zeichen ';' aufsplitten
         if temp[2] == "Insgesamt":
-            prozentualeErzeugung.append(int(temp2) / int(temp[3]))
             gesamtErzeugung.append(int(temp[3]))
             jahre.append(datetime.date(int(temp[0]), monthToInt(temp[1]), 1))
         elif temp[2] == "Steinkohlen":
-            temp2 = temp[3]
+            steinkohleErzeugung.append(int(temp[3]))
+            
+i = 0
+while i < len(steinkohleErzeugung):
+    prozentualeErzeugung.append(int(steinkohleErzeugung[i]) / int(gesamtErzeugung[i]))
+    i += 1
 
 data.close()
 
@@ -54,9 +59,12 @@ data.close()
 
 fig, ax1 = plt.subplots()
 
-ax1.set_xlabel("Jahr")                                  #x-Achsen name hinzufügen
+ax1.set_xlabel("Jahr")                                  #x-Achsen Name hinzufügen
 ax1.set_ylabel("Gesamtenergieproduktion in GW")
-ax1.plot(jahre, gesamtErzeugung, '-o')                  #Plot erstellen aus der x-Achse 'jahre' und der y-Achse 'gesamtErzeugung' mit Punkten an Wertestellen
+ax1.plot(jahre, steinkohleErzeugung, '-ob', label='Steinkohlestrom')
+ax1.plot(jahre, gesamtErzeugung, '-og', label='Gesamtstrom')
+
+plt.legend(loc="upper right")
 
 ax1.xaxis.set_major_locator(mpl_dates.YearLocator())
 ax1.xaxis.set_major_formatter(mpl_dates.DateFormatter('%Y'))    #Datumsformat definiert, siehe https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
@@ -65,11 +73,14 @@ ax1.xaxis.set_minor_locator(mpl_dates.MonthLocator())
 ax2 = ax1.twinx()
 
 ax2.set_ylabel("Kohlestrom prozentual an Gesamtproduktion", color='tab:red')
-ax2.plot(jahre, prozentualeErzeugung, '-or')
+ax2.plot(jahre, prozentualeErzeugung, '-or', label='Kohlestrom prozentual an Gesamtproduktion')
 ax2.tick_params(axis='y', labelcolor='tab:red')
 ax2.yaxis.set_major_formatter(mpl_tick.PercentFormatter(1.0, 0))  #y-Achse von Graph 2 auf Prozent einstellen für Werte von 0 bis 1 mit 0 Stellen nach dem Komma
 
+
+plt.legend()
+
 fig.tight_layout()
 
-plt.gcf().autofmt_xdate()                              #y-Achsen Beschriftung anwinkeln
+plt.gcf().autofmt_xdate()                              #x-Achsen Beschriftung anwinkeln
 plt.show()
